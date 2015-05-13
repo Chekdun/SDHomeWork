25db75175eb871e6cbe5165880ce59df52d9be39
Denis Sidorov <chekdun@MacBook-Air-Irina.local>
2015-05-13 18:44:44 +0300

    Initial Commit

---
+++ DSStudy/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DSStudy
+//
+//  Created by Denis Sidorov on 13.05.15.
+//  Copyright (c) 2015 DenisSidorov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
