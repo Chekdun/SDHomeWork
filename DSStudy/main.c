550e00fea085d1fe9922cdb60c723727246cc01a
Denis Sidorov <chekdun@gmail.com>
2015-05-20 23:49:15 +0300

    verification

--- DSStudy/main.c
+++ DSStudy/main.c
@@ -1,15 +1,14 @@
 //
-//  main.c
 //  DSStudy
 //
-//  Created by Denis Sidorov on 13.05.15.
+//  Created by Denis Sidorov on 20.05.15.
 //  Copyright (c) 2015 DenisSidorov. All rights reserved.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    
     return 0;
 }
